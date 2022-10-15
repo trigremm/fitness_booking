70f7d14da2a24a2e00f60369190a61a2623cc065
Molkenov Askhat <askhat.molkenov@gmail.com>
2022-10-15 16:26:47 +0600

    get to post

--- backend/users/urls_auth.py
+++ backend/users/urls_auth.py
@@ -6,7 +6,7 @@ from users.views import AnonymousUserViewSet
 urlpatterns = [
     path("register/", AnonymousUserViewSet.as_view({"post": "user_registration"})),
     path("activate/", AnonymousUserViewSet.as_view({"get": "user_activation"})),
-    path("forgot_password/", AnonymousUserViewSet.as_view({"get": "user_forgot_password"})),
+    path("forgot_password/", AnonymousUserViewSet.as_view({"post": "user_forgot_password"})),
     path("reset_password/", AnonymousUserViewSet.as_view({"post": "user_reset_password"})),
     path("refresh/", TokenRefreshView.as_view(), name="refresh_token"),
     path("login/", TokenObtainPairView.as_view(), name="obtain_auth_token_pair"),
